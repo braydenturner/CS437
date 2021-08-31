094b8cf4bc28274466333d4928cb80df51d1fdf2
Brayden Turner <brayden@apple.com>
2021-08-31 16:44:54 -0700

    ultrasonic readings

--- lab1/mapping.py
+++ lab1/mapping.py
@@ -68,7 +68,7 @@ class Ultrasonic:
     @staticmethod
     def mark_point(point: [int, int]):
         global side_length
-        x, y = np.round(point[0]), np.round(point[1])
+        x, y = int(point[0]), int(point[1])
 
         if x < side_length and y < side_length:
             print(f"Marking point({x},{y})")
