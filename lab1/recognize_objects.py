d2bc95099fce1d16aba036307901390556920990
Brayden Turner <brayden@apple.com>
2021-08-31 20:48:36 -0700

    print object class

--- lab1/recognize_objects.py
+++ lab1/recognize_objects.py
@@ -3,6 +3,7 @@ from tflite_runtime.interpreter import Interpreter
 from PIL import Image
 import io
 import picamera
+import re
 
 
 CAMERA_WIDTH = 640
@@ -14,7 +15,22 @@ class ObjectRecognition:
     def __init__(self, model="/tmp/detect.tflite"):
         self.interpreter = Interpreter("/tmp/detect.tflite")
         self.interpreter.allocate_tensors()
+        self.labels = ObjectRecognition.load_labels("/tmp/coco_labels.txt")
         _, self.input_height, self.input_width, _ = self.interpreter.get_input_details()[0]['shape']
+
+    @staticmethod
+    def load_labels(path):
+        """Loads the labels file. Supports files with or without index numbers."""
+        with open(path, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+            labels = {}
+            for row_number, content in enumerate(lines):
+                pair = re.split(r'[:\s]+', content.strip(), maxsplit=1)
+                if len(pair) == 2 and pair[0].strip().isdigit():
+                    labels[int(pair[0])] = pair[1].strip()
+                else:
+                    labels[row_number] = pair[0].strip()
+        return labels
 
     def set_input_tensor(self, image):
         """Sets the input tensor."""
@@ -50,7 +66,6 @@ class ObjectRecognition:
                 results.append(result)
         return results
 
-
     def detect(self):
         with picamera.PiCamera(resolution=(CAMERA_WIDTH, CAMERA_HEIGHT), framerate=30) as camera:
             stream = io.BytesIO()
@@ -59,7 +74,8 @@ class ObjectRecognition:
                 image = Image.open(stream).convert('RGB').resize(
                     (self.input_width, self.input_height), Image.ANTIALIAS)
                 results = self.detect_objects(image, 0.4)
-                print(results)
+                objects = [(self.labels[result['class_id']], result['score']) for result in results]
+                print(objects)
                 stream.seek(0)
                 stream.truncate()
 
