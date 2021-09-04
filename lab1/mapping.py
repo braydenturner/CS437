fc90522aa4c8c2e62e3b5922d7edcf4f2273dbba
Brayden Turner <brayden@apple.com>
2021-09-04 16:10:14 -0700

    fix move computation

--- lab1/mapping.py
+++ lab1/mapping.py
@@ -273,7 +273,7 @@ class Movement:
         forward = Movement.Move(Movement.Move.Type.Forward, 0)
         moves = []
         for next_point in path:
-            if next_point is None and last_point is None:
+            if next_point is None or last_point is None:
                 last_point = next_point
                 continue
             # Still forward
