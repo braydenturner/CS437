450373f53126b41eef183c050a394e432291739d
Brayden Turner <brayden_turner@apple.com>
2021-09-04 18:54:29 -0700

    turn time

--- lab1/mapping.py
+++ lab1/mapping.py
@@ -199,7 +199,7 @@ class Ultrasonic:
         '''
         global world_map
         padded_map = np.copy(world_map)
-        for x in range(15):
+        for x in range(12):
             temp_map = np.copy(padded_map)
             for row_i, row in enumerate(padded_map):
                 for col_i, col in enumerate(row):
